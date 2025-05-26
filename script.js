a243aec42521e68c5b9d66befd19d70ad9c4ef40
Saurav Singh <ss0912@srmist.edu.in>
2025-05-26 20:21:52 +0530

    Final Changes Made

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function beforeSubmit(){
-    let outputDate = document.querySelector('.outputDate');
-    let inputDate = document.querySelector('.inputDate');
-    console.log('Input Date--> ',inputDate.value);//string-->Date(en_IN)
+let captchaChecked = false;
+if(captchaChecked){
+    function beforeSubmit(event){
+        let outputDate = document.querySelector('.outputDate');
+        let inputDate = document.querySelector('.inputDate');
+        console.log('Input Date--> ',inputDate.value);//string-->Date(en_IN)
 
-    let formattedDate = new Date(inputDate.value).toLocaleDateString('en-IN'); 
-    outputDate.value = formattedDate;
+        let formattedDate = new Date(inputDate.value).toLocaleDateString('en-IN'); 
+        outputDate.value = formattedDate;
+    }
 }
+else{
+    alert("Please complete the CAPTCHA before submitting the form.");
+    event.preventDefault();
+    
+}
+
 function timestamp() { 
     var response = document.getElementById("g-recaptcha-response"); 
     if (response == null || response.value.trim() == "") {
@@ -15,4 +24,9 @@ function timestamp() {
     } 
 } 
 setInterval(timestamp, 500);
+
+function captchaSuccess(){
+    captchaChecked = true;
+}
+    
 
